99715de5dabc10fae4cd1167eed15c522a8c0809
A-Ayerh <aayerh@asu.edu>
2025-09-02 22:13:21 -0700

    code generation statrted for mem and shim movement

--- capstone/andrew/Task_1.py
+++ capstone/andrew/Task_1.py
@@ -16,9 +16,31 @@ from aie.iron.device.tile import AnyComputeTile
 from aie.helpers.taplib.tap import TensorAccessPattern
 import aie.iron as iron
 
-def allocate_mem_tiles(data_ty, size, fifo_name):
-    of_in = ObjectFifo(data_ty,)
-    of_mem = of_in.cons().forward(name=fifo_name)
+# Function to dynamically generate object fifos for shim-> mem tiles depending on how much data the user wants to stream in.
+def allocate_in_mem_tiles(data_ty, size, fifo_name):
+    fifos = {}
+    for i in range(1, size//(512*i)):
+        if i == 5:
+            print("datasize exceeds memory shared across L2")
+            break
+        fifos[i+"of_in"] = ObjectFifo(data_ty, name=fifo_name+"_in_"+"{i}")
+        fifos[i+"of_mem_in"] = fifos[i+"of_in"].cons().forward(name=fifo_name+"_in_mem_"+"{i}")
+        
+    
+    return fifos
+
+# Function to dynamically generate object fifos for mem -> shim tiles depending on how much data the user wants to stream out.
+def allocate_out_mem_tiles(data_ty, size, fifo_name):
+    fifos = {}
+    for i in range(1, size//(512*i)):
+        if i == 5:
+            print("datasize exceeds memory shared across L2")
+            break
+        fifos[i+"of_mem_out"] = ObjectFifo(data_ty, name=fifo_name+"_out_mem_"+"{i}")
+        fifos[i+"of_out"] = fifos[i+"of_mem_out"].cons().forward(name=fifo_name+"_out_"+"{i}")
+        
+    
+    return fifos
 
 
 
@@ -28,13 +50,30 @@ def exercise_5a(input0, output):
     element_type = input0.dtype
 
     data_ty = np.ndarray[(data_size,), np.dtype[element_type]]
+    
+    #TODO:generate object fifos to handle amount of data:
+    #Shim -> Mem tiles
+    fifos_in = allocate_in_mem_tiles(data_ty, data_size, "a")
+    print(fifos_in[0])
+    #Mem tiles -> shim
+    fifos_out = allocate_out_mem_tiles(data_ty, data_size, "a")
+    print(fifos_out[0])
+    
+    # Shim -> Compute tile
+    # Compute tile -> Shim
 
+    #Mem tile -> Compute tile:
+    #Compute Tile -> Mem tile:
+
+    
+
+    
      # Dataflow with ObjectFifos
-    of_in = ObjectFifo(data_ty, name="in")
-    of_in_mem = of_in.cons().forward(name="in_mem")
+    # of_in = ObjectFifo(data_ty, name="in")
+    # of_in_mem = of_in.cons().forward(name="in_mem")
 
-    of_out_mem = ObjectFifo(data_ty, name="out")
-    of_out = of_out_mem.cons().forward(name="out_mem")
+    # of_out_mem = ObjectFifo(data_ty, name="out")
+    # of_out = of_out_mem.cons().forward(name="out_mem")
     
 
     # Task for the core to perform
@@ -47,14 +86,14 @@ def exercise_5a(input0, output):
         of_in.release(1)
 
     # Create a worker to perform the task
-    my_worker = Worker(core_fn, [of_in_mem.cons(), of_out_mem.prod()])
+    #my_worker = Worker(core_fn, [of_in_mem.cons(), of_out_mem.prod()])
 
     # To/from AIE-array runtime data movement
     rt = Runtime()
     with rt.sequence(data_ty, data_ty) as (a_in, c_out):
-        rt.start(my_worker)
-        rt.fill(of_in.prod(), a_in)
-        rt.drain(of_out.cons(), c_out, wait=True)
+        # rt.start(my_worker)
+        rt.fill(fifos_in[0].prod(), a_in)
+        rt.drain(fifos_out[0].cons(), c_out, wait=True)
 
     # Create the program from the device type and runtime
     my_program = Program(iron.get_current_device(), rt)
@@ -62,7 +101,7 @@ def exercise_5a(input0, output):
     # Place components (assign them resources on the device) and generate an MLIR module
     
     my_program=  my_program.resolve_program(SequentialPlacer())
-    print(my_program)
+    #print(my_program)
 
     return my_program
 
